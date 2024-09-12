e9ce5749627a3d3d597f392026c3bf8c7c885a8f
Jess <shijessica9@gmail.com>
2024-09-11 21:25:46 -0400

    add project

---
+++ README.md
@@ -0,0 +1,140 @@
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/Anonymous893/ChessAI">
+    <img src="images/logo.png" alt="Logo" width="80" height="80">
+  </a>
+
+<h3 align="center">ChessAI</h3>
+
+  <p align="center">
+    A simple Chess AI
+    <br />
+    <a href="https://github.com/Anonymous893/ChessAI"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    <a href="https://github.com/Anonymous893/ChessAI">View Demo</a>
+    ·
+    <a href="https://github.com/Anonymous893/ChessAI/issues/new?labels=bug&template=bug-report---.md">Report Bug</a>
+    ·
+    <a href="https://github.com/Anonymous893/ChessAI/issues/new?labels=enhancement&template=feature-request---.md">Request Feature</a>
+  </p>
+</div>
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#roadmap">Roadmap</a></li>
+    <li><a href="#acknowledgments">Acknowledgments</a></li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+[![Product Name Screen Shot][product-screenshot]](https://example.com)
+
+This project is a simple Chess AI where the player competes against the computer. The AI uses the minimax algorithm with alpha-beta pruning to efficiently calculate its next moves. Players follow the standard rules of chess as they challenge the AI's strategic gameplay.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+### Built With
+
+* [![JS][JavaScript]][JS-url]
+* [![HTML][HTML5]][HTML-url]
+* [![CSS][CSS5]][CSS-url]
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+### Prerequisites
+
+To get things running locally. follow these simple steps.
+
+### Installation
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/Anonymous893/ChessAI.git
+   ```
+2. Install NPM packages
+   ```sh
+   npm install
+   ```
+3. Install dependencies inside the repo
+   ```js
+   npm init
+   ```
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+Click on the "Start Game" button to begin.
+
+![Alt text](https://github.com/Anonymous893/ChessAI/blob/main/Images/chessIntro.gif)
+
+Then, play the game with the AI based on the rules of standard chess. A short demo
+is shown below.
+
+![Alt text](https://github.com/Anonymous893/Chess/blob/main/Images/chessPlay.gif)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- ROADMAP -->
+## Roadmap
+
+- [ ] Fix minor bug
+    - [ ] Rarely, a piece is not allowed to be moved into a valid position.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- ACKNOWLEDGMENTS -->
+## Acknowledgments
+
+* [Chess.js Library](https://github.com/jhlywa/chess.js/blob/master/README.md)
+* [Chessboard.js Library](https://chessboardjs.com/)
+* [Project Inspiration](https://github.com/lhartikk/simple-chess-ai)
+* [Chess Programming Wiki](https://www.chessprogramming.org/Main_Page)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+[product-screenshot]: images/screenshot.png
+
+[JavaScript]: https://img.shields.io/badge/JavaScript-FFD1DC?style=for-the-badge&logo=javascript&logoColor=black
+[JS-url]: https://www.javascript.com/
+[HTML5]: https://img.shields.io/badge/HTML-957DAD?style=for-the-badge&logo=html5&logoColor=white
+[HTML-url]: https://html.com/
+[CSS5]: https://img.shields.io/badge/CSS-A2BFFE?&style=for-the-badge&logo=css3&logoColor=white
+[CSS-url]: https://developer.mozilla.org/en-US/docs/Web/CSS
---
+++ script.js
@@ -0,0 +1,318 @@
+var board, game = new Chess();
+
+
+//Decides the best move to play using the list of moves from minimax
+var moveToPlay =function(depth, game, isMaximisingPlayer) {
+
+    //Generate all possible moves and initialise best move
+    var newGameMoves = game.ugly_moves();
+    var bestMove = -Infinity;
+    var bestMoveFound;
+
+    for(var i = 0; i < newGameMoves.length; i++) {
+        var newGameMove = newGameMoves[i]
+        game.ugly_move(newGameMove);
+        var value = minimax(depth - 1, game, -10000, 10000, !isMaximisingPlayer);
+        game.undo();
+        if(value >= bestMove) {
+            bestMove = value;
+            bestMoveFound = newGameMove;
+        }
+    }
+    return bestMoveFound;
+};
+
+//Minimax algorithm with alpha-beta pruning
+const minimax = (depth, game, alpha, beta, isMaximisingPlayer) => {
+    positionCount++;
+
+    //Base case: Evaluate the board at the maximum search depth
+    if (depth === 0) {
+        return evaluateBoard(game.board());
+    }
+
+    const newGameMoves = game.ugly_moves();
+
+    //Recursive helper function
+    const minimaxHelper = (isMaximisingPlayer, bestMoveInit, compareFunc, alphaFunc, betaFunc) => {
+        let bestMove = bestMoveInit;
+
+        //Loop through all possible moves
+        for (const move of newGameMoves) {
+            game.ugly_move(move);
+            const value = minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer);
+            game.undo();
+
+            bestMove = compareFunc(bestMove, value);
+
+            //Update alpha or beta value based on the current player's turn
+            if (isMaximisingPlayer) {
+                alpha = alphaFunc(alpha, bestMove);
+            } else {
+                beta = betaFunc(beta, bestMove);
+            }
+
+            //Alpha-beta pruning: If the current branch can no longer improve the outcome, 
+            //stop further evaluation
+            if (beta <= alpha) break;
+        }
+
+        return bestMove;
+    };
+
+    // Maximising player's turn
+    if (isMaximisingPlayer) {
+        return minimaxHelper(true, -Infinity, Math.max, Math.max, Math.min);
+    }
+    // Minimising player's turn
+    else {
+        return minimaxHelper(false, Infinity, Math.min, Math.min, Math.max);
+    }
+};
+
+var evaluateBoard = function (board) {
+    var totalEvaluation = 0;
+    for (var i = 0; i < 8; i++) {
+        for (var j = 0; j < 8; j++) {
+            totalEvaluation = totalEvaluation + getPieceValue(board[i][j], i ,j);
+        }
+    }
+    return totalEvaluation;
+};
+
+var reverseArray = function(array) {
+    return array.slice().reverse();
+};
+
+/* Piece-square tables for evaluation: Values from the chess programming wiki */
+var pawnEvalWhite =
+    [
+        [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
+        [5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0],
+        [1.0,  1.0,  2.0,  3.0,  3.0,  2.0,  1.0,  1.0],
+        [0.5,  0.5,  1.0,  2.5,  2.5,  1.0,  0.5,  0.5],
+        [0.0,  0.0,  0.0,  2.0,  2.0,  0.0,  0.0,  0.0],
+        [0.5, -0.5, -1.0,  0.0,  0.0, -1.0, -0.5,  0.5],
+        [0.5,  1.0, 1.0,  -2.0, -2.0,  1.0,  1.0,  0.5],
+        [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0]
+    ];
+
+var pawnEvalBlack = reverseArray(pawnEvalWhite);
+
+var bishopEvalWhite = [
+    [ -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
+    [ -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [ -1.0,  0.0,  0.5,  1.0,  1.0,  0.5,  0.0, -1.0],
+    [ -1.0,  0.5,  0.5,  1.0,  1.0,  0.5,  0.5, -1.0],
+    [ -1.0,  0.0,  1.0,  1.0,  1.0,  1.0,  0.0, -1.0],
+    [ -1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0, -1.0],
+    [ -1.0,  0.5,  0.0,  0.0,  0.0,  0.0,  0.5, -1.0],
+    [ -2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0]
+];
+
+var bishopEvalBlack = reverseArray(bishopEvalWhite);
+
+var rookEvalWhite = [
+    [  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0],
+    [  0.5,  1.0,  1.0,  1.0,  1.0,  1.0,  1.0,  0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [ -0.5,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -0.5],
+    [  0.0,   0.0, 0.0,  0.5,  0.5,  0.0,  0.0,  0.0]
+];
+
+var rookEvalBlack = reverseArray(rookEvalWhite);
+
+var knightEval =
+    [
+        [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0],
+        [-4.0, -2.0,  0.0,  0.0,  0.0,  0.0, -2.0, -4.0],
+        [-3.0,  0.0,  1.0,  1.5,  1.5,  1.0,  0.0, -3.0],
+        [-3.0,  0.5,  1.5,  2.0,  2.0,  1.5,  0.5, -3.0],
+        [-3.0,  0.0,  1.5,  2.0,  2.0,  1.5,  0.0, -3.0],
+        [-3.0,  0.5,  1.0,  1.5,  1.5,  1.0,  0.5, -3.0],
+        [-4.0, -2.0,  0.0,  0.5,  0.5,  0.0, -2.0, -4.0],
+        [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0]
+    ];
+
+var evalQueen = [
+    [ -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
+    [ -1.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [ -1.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
+    [ -0.5,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
+    [  0.0,  0.0,  0.5,  0.5,  0.5,  0.5,  0.0, -0.5],
+    [ -1.0,  0.5,  0.5,  0.5,  0.5,  0.5,  0.0, -1.0],
+    [ -1.0,  0.0,  0.5,  0.0,  0.0,  0.0,  0.0, -1.0],
+    [ -2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0]
+];
+
+var kingEvalWhite = [
+
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+    [ -2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0],
+    [ -1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0],
+    [  2.0,  2.0,  0.0,  0.0,  0.0,  0.0,  2.0,  2.0 ],
+    [  2.0,  3.0,  1.0,  0.0,  0.0,  1.0,  3.0,  2.0 ]
+];
+
+var kingEvalBlack = reverseArray(kingEvalWhite);
+
+var getPieceValue = function (piece, x, y) {
+    if (piece === null) {
+        return 0;
+    }
+
+    // Define the base values for each piece type
+    const baseValues = {
+        'p': 10,
+        'r': 50,
+        'n': 30,
+        'b': 30,
+        'q': 90,
+        'k': 900
+    };
+
+    // Define piece-specific evaluation tables
+    const evalTables = {
+        'p': piece.color === 'w' ? pawnEvalWhite : pawnEvalBlack,
+        'r': piece.color === 'w' ? rookEvalWhite : rookEvalBlack,
+        'n': knightEval,
+        'b': piece.color === 'w' ? bishopEvalWhite : bishopEvalBlack,
+        'q': evalQueen,
+        'k': piece.color === 'w' ? kingEvalWhite : kingEvalBlack
+    };
+
+    const { type, color } = piece;
+    const baseValue = baseValues[type];
+    const evalTable = evalTables[type];
+
+    if (baseValue === undefined || evalTable === undefined) {
+        throw "Unknown piece type: " + type;
+    }
+
+    // Calculate the absolute value of the piece
+    const absoluteValue = baseValue + evalTable[y][x];
+    return color === 'w' ? absoluteValue : -absoluteValue;
+};
+
+/* Board Visualisation and Game State Handling */
+var onDragStart = function (piece) {
+    if (game.in_checkmate() === true || game.in_draw() === true ||
+        piece.search(/^b/) !== -1) {
+        return false;
+    }
+};
+
+var makeBestMove = function () {
+    var bestMove = getBestMove(game);
+    game.ugly_move(bestMove);
+    board.position(game.fen());
+    renderMoveHistory(game.history());
+    if (game.game_over()) {
+        alert('Game over');
+    }
+};
+
+
+var positionCount;
+var getBestMove = function (game) {
+    if (game.game_over()) {
+        alert('Game over');
+    }
+
+    positionCount = 0;
+    var depth = parseInt($('#search-depth').find(':selected').text());
+
+    var d = new Date().getTime();
+    var bestMove = moveToPlay(depth, game, true);
+    var d2 = new Date().getTime();
+    var moveTime = (d2 - d);
+    var positionsPerS = ( positionCount * 1000 / moveTime);
+
+    $('#position-count').text(positionCount);
+    $('#time').text(moveTime/1000 + 's');
+    $('#positions-per-s').text(positionsPerS);
+    return bestMove;
+};
+
+var renderMoveHistory = function (moves) {
+    var historyElement = $('#move-history').empty();
+    historyElement.empty();
+    for (var i = 0; i < moves.length; i = i + 2) {
+        historyElement.append('<span>' + moves[i] + ' ' + ( moves[i + 1] ? moves[i + 1] : ' ') + '</span><br>')
+    }
+    historyElement.scrollTop(historyElement[0].scrollHeight);
+
+};
+
+var onDrop = function (source, target) {
+
+    var move = game.move({
+        from: source,
+        to: target,
+        promotion: 'q'
+    });
+
+    removeGreySquares();
+    if (move === null) {
+        return 'snapback';
+    }
+
+    renderMoveHistory(game.history());
+    window.setTimeout(makeBestMove, 250);
+};
+
+var onSnapEnd = function () {
+    board.position(game.fen());
+};
+
+var onMouseoverSquare = function(square, piece) {
+    var moves = game.moves({
+        square: square,
+        verbose: true
+    });
+
+    if (moves.length === 0) return;
+
+    greySquare(square);
+
+    for (var i = 0; i < moves.length; i++) {
+        greySquare(moves[i].to);
+    }
+};
+
+var onMouseoutSquare = function(square, piece) {
+    removeGreySquares();
+};
+
+var removeGreySquares = function() {
+    $('#board .square-55d63').css('background', '');
+};
+
+var greySquare = function(square) {
+    var squareEl = $('#board .square-' + square);
+
+    var background = '#a9a9a9';
+    if (squareEl.hasClass('black-3c85d') === true) {
+        background = '#696969';
+    }
+
+    squareEl.css('background', background);
+};
+
+var cfg = {
+    draggable: true,
+    position: 'start',
+    onDragStart: onDragStart,
+    onDrop: onDrop,
+    onMouseoutSquare: onMouseoutSquare,
+    onMouseoverSquare: onMouseoverSquare,
+    onSnapEnd: onSnapEnd
+};
+
+board = ChessBoard('board', cfg);
